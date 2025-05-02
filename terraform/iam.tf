21cba090ee3fc0188488af69f683120263f9763b
ASUS <naazv05@gmail.com>
2025-05-02 13:30:22 +0530

    iam rules

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,38 +1,38 @@
-# resource "aws_iam_role" "ec2_role" {
-#   name               = "worldwithweb-ec2-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action    = "sts:AssumeRole"
-#         Effect    = "Allow"
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+resource "aws_iam_role" "ec2_role" {
+  name               = "worldwithweb-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
 
-# resource "aws_iam_policy" "ec2_secrets_policy" {
-#   name        = "worldwithweb-ec2-secrets-policy"
-#   description = "Allows access to Secrets Manager"
-#   policy      = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action    = [
-#           "secretsmanager:GetSecretValue",
-#           "secretsmanager:DescribeSecret"
-#         ]
-#         Effect    = "Allow"
-#         Resource  = "*"
-#       },
-#     ]
-#   })
-# }
+resource "aws_iam_policy" "ec2_secrets_policy" {
+  name        = "worldwithweb-ec2-secrets-policy"
+  description = "Allows access to Secrets Manager"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Effect    = "Allow"
+        Resource  = "*"
+      },
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "attach_policy" {
-#   role       = aws_iam_role.ec2_role.name
-#   policy_arn = aws_iam_policy.ec2_secrets_policy.arn
-# }
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.ec2_secrets_policy.arn
+}
